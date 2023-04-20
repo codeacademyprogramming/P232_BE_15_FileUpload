405abaf7bf3a717111f9123fb143e9cee5e6d03a
eaghayev <eaghayevv@gmail.com>
2023-04-20 17:50:27 +0400

    project added

---
+++ Pustok/Pustok/Helpers/FileManager.cs
@@ -0,0 +1,20 @@
+﻿using Pustok.Models;
+
+namespace Pustok.Helpers
+{
+    public static class FileManager
+    {
+        public static string Save(IFormFile file, string folder)
+        {
+            string newFileName = Guid.NewGuid().ToString() + file.FileName;
+            string path = Path.Combine(folder, newFileName);
+
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            return newFileName;
+        }
+    }
+}
